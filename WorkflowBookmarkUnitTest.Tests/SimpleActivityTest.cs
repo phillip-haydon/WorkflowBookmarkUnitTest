a7b468d4fbd46877a1979be20d5bdf7f8fc3d776
Phillip Haydon <phillip.haydon@gmail.com>
2013-12-18 00:51:21 +0800

    commited for blog post

---
+++ WorkflowBookmarkUnitTest.Tests/SimpleActivityTest.cs
@@ -0,0 +1,63 @@
+﻿namespace WorkflowBookmarkUnitTest.Tests
+{
+    using Microsoft.Activities.UnitTesting;
+    using System.Linq;
+    using WorkflowBookmarkUnitTest;
+    using Xunit;
+
+    public class SimpleActivityTest
+    {
+        [Fact]
+        public void Given_10_For_Value_Two_Should_Have_A_Return_Argument_Of_True()
+        {
+            // Arrange
+            var wfTester = WorkflowApplicationTest.Create(new SampleActivity
+            {
+                ValueOne = 123,
+                ValueTwo = 10
+            });
+
+            // Act
+            wfTester.TestActivity();
+
+            // Assert
+            wfTester.AssertOutArgument.IsTrue("Result");
+        }
+
+        [Fact]
+        public void Given_9_For_Value_Two_Should_Have_Bookmark_Using_Value_One()
+        {
+            // Arrange
+            var wfTester = WorkflowApplicationTest.Create(new SampleActivity
+            {
+                ValueOne = 123,
+                ValueTwo = 9
+            });
+
+            // Act
+            wfTester.TestActivity();
+            wfTester.TestWorkflowApplication.GetBookmarks();
+
+            // Assert
+            Assert.Equal("Bookmark_123", wfTester.Bookmarks.First());
+        }
+
+        [Fact]
+        public void When_Resuming_A_Workflow_Should_Get_The_Return_Value_False()
+        {
+            // Arrange
+            var wfTester = WorkflowApplicationTest.Create(new SampleActivity
+            {
+                ValueOne = 123,
+                ValueTwo = 9
+            });
+
+            // Act
+            wfTester.TestActivity();
+            wfTester.TestWorkflowApplication.ResumeBookmark("Bookmark_123", 0);
+
+            // Assert
+            wfTester.AssertOutArgument.IsFalse("Result");
+        }
+    }
+}
---
+++ WorkflowBookmarkUnitTest/SampleActivity.cs
@@ -0,0 +1,38 @@
+﻿namespace WorkflowBookmarkUnitTest
+{
+    using System.Activities;
+
+    public class SampleActivity : NativeActivity<bool>
+    {
+        public InArgument<int> ValueOne { get; set; }
+        public InArgument<int> ValueTwo { get; set; }
+
+        protected override bool CanInduceIdle
+        {
+            get { return true; }
+        }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            var valueOne = context.GetValue(ValueOne);
+            var valueTwo = context.GetValue(ValueTwo);
+
+            if (valueTwo % 2 == 0)
+            {
+                //We do something and return true...
+                context.SetValue(Result, true);
+
+                return;
+            }
+
+            // Else condition was not met so we sleep and come back later
+            context.CreateBookmark("Bookmark_" + valueOne, ResumeBookmarkCallback);
+        }
+
+        private void ResumeBookmarkCallback(NativeActivityContext context, Bookmark bookmark, object value)
+        {
+            // When we wake up, return false and let it try again
+            context.SetValue(Result, false);
+        }
+    }
+}
